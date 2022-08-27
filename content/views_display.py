c76e20fe67f728b611b85a97a29e0d0ac2760912
Breit <4144192+Breit@users.noreply.github.com>
2022-08-27 18:54:26 +0200

    Race monitor displays current race block

--- content/views_display.py
+++ content/views_display.py
@@ -1,9 +1,57 @@
+from constance import config
 from django.shortcuts import render
 
-from .views_times import getRaceResultsTableContent
+from .views_times import getRaceTimes
+from .views_timetable import combineTimeOffset
 from .views_main import getSiteData
+
+
+def getCurrentTimeTable():
+    timetable = None
+
+    # get current heats
+    for i in range(config.heatCount):
+        races = getRaceTimes('{}{}-'.format(config.heatPrefix, i + 1))
+        if len(races) > 0 and all(['lanes' in race for race in races]):
+            if all([all(lane['time'] > 0.0 for lane in race['lanes']) for race in races]):
+                # current heat already complete
+                continue
+
+            timetable = {
+                    'time': races[0]['time'] if len(races) > 0 else combineTimeOffset(
+                        config.timeBegin,
+                        config.offsetHeat
+                    ),
+                    'desc': '{} {}'.format(config.heatsTitle, i + 1),
+                    'races': races,
+                    'type': 'heat'
+                }
+            break
+
+    # get final if heats are finished
+    if timetable is None:
+        races = getRaceTimes(config.finalPrefix)
+        timetable = {
+                'time': races[0]['time'] if len(races) > 0 else combineTimeOffset(
+                    config.timeBegin,
+                    config.offsetFinale
+                ),
+                'desc': config.finaleTitle,
+                'races': races,
+                'type': 'finale'
+            }
+
+    return timetable
+
+def getRankings():
+    rankings = []
+    # TODO
+    return rankings
 
 def display(request):
     siteData = getSiteData('display')
-    siteData['display'] = getRaceResultsTableContent()
+    siteData['display'] = {
+        'timetable': getCurrentTimeTable(),
+        'rankings': getRankings()
+    }
     return render(request, 'display.html', siteData)
