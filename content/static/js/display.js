b85cb2e2cfbf96288694812d30df1af179c83312
Martin Breitbarth <martin.breitbarth@cognex.com>
2024-09-10 10:14:25 +0200

    Disable cursor and curser interactions on display (race monitor) content panel

--- content/static/js/display.js
+++ content/static/js/display.js
@@ -1,17 +1,47 @@
+var mouseTimer = null;
+var cursorVisible = true;
+
+function disappearCursor()
+{
+    mouseTimer = null;
+    document.body.style.cursor = "none";
+    cursorVisible = false;
+}
+
 $(document).ready(
     function()
     {
         window.setInterval(
-            function() {
+            function()
+            {
                 $("#liveClock").html(new Date().toLocaleTimeString('de-DE'));
             },
             1000
         );
         window.setInterval(
-            function() {
+            function()
+            {
                 $("#resultsCarousel").load(window.location.href + " #resultsCarousel");
             },
             displayDataRefresh
         );
+
+        document.onmousemove = function()
+        {
+            if (mouseTimer)
+            {
+                window.clearTimeout(mouseTimer);
+            }
+            if (!cursorVisible)
+            {
+                document.body.style.cursor = "default";
+                cursorVisible = true;
+            }
+            mouseTimer = window.setTimeout(disappearCursor, 1000);
+        };
+
+        window.setTimeout(disappearCursor, 100);
     }
 );
+
+
